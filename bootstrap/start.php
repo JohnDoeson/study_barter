6f3b021457ff5457bc36a48f220ad67bec891bb7
ANSH <ggarmon@ukr.net>
2015-09-20 17:50:39 +0300

    Pc name change

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('Ruslan-PC','ANSH-ПК'),
+	'local' => array('Ruslan-PC','ANSH-PC'),
 
 ));
 
