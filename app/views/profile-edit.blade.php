4b05e24ff8e3586c69f3f3fc0459ec1566107da5
ANSH <ggarmon@ukr.net>
2015-09-20 22:30:21 +0300

    Страничка пользователя
    скрипты лежат в пхп файле ибо из кастом.дж не подгружаются.

--- app/views/profile-edit.blade.php
+++ app/views/profile-edit.blade.php
@@ -13,7 +13,8 @@
 	            @endforeach
 	        </div>
 	    @endif
-	    <h1>Мой профиль<sup>Реп.({{$rep}})</sup></h1>
+	    <h1>Мой профиль</h1>
+	    <h4 class="rep">Репутация: {{$rep}}</h4>
 	    @if($is_vk)
 	    	<div class="alert alert-warning">
 	    		<p>Для того чтобы изменить имя, фамилию, аватар, просто отредактируйте свой профиль Vkontakte</p>
@@ -26,9 +27,9 @@
 	    		@endif
 	    	</div>
 		    <div class="col-md-10">
-			    <div class="form-group">
-		        	<label class="col-sm-2 control-label" for="formGroupName">Имя</label>
-		        	<div class="col-sm-10">
+			    <div class="form-group ">
+		        	<label class="col-sm-3 control-label" for="formGroupName">Имя</label>
+		        	<div class="col-sm-9 profile_group">
 			        	@if($is_vk)
 			        		<p>{{ $name }}</p>
 			        	@else
@@ -36,9 +37,9 @@
 			        	@endif
 		        	</div>
 		        </div>
-		        <div class="form-group">
-		        	<label class="col-sm-2 control-label" for="formGroupSurname">Фамилия</label>
-		        	<div class="col-sm-10">
+		        <div class="form-group ">
+		        	<label class="col-sm-3 control-label" for="formGroupSurname">Фамилия</label>
+		        	<div class="col-sm-9 profile_group">
 		        		@if($is_vk)
 		        			<p>{{ $surname }}</p>
 		        		@else
@@ -46,9 +47,9 @@
 		        		@endif
 		        	</div>
 		        </div>
-		        <div class="form-group">
-		        	<label class="col-sm-2 control-label" for="formGroupSurname">Населенный пункт</label>
-		        	<div class="col-sm-10">
+		        <div class="form-group ">
+		        	<label class="col-sm-3 control-label" for="formGroupSurname">Населенный пункт</label>
+		        	<div class="col-sm-9 profile_group">
 		        		<div class="col-sm-5" style="padding: 0px;" >
 			        		<select name="region" id="regionSelect" class="form-control select select-primary select-block mbl">
 			        			<option value="0">Не выбран</option>
@@ -75,4 +76,155 @@
 		    </div>
 		</div>	
 	</div>
+	
+<!--	Выбор профессии-->
+<section class="profile_skills">
+    <div class="container prof_select">
+        <div class="row skills">
+            <div class="col-md-6 text-center">
+                <h3>Умею:</h3>
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Добавить" id="search-query-3">
+                      <span class="input-group-btn">
+                        <button type="submit" class="btn"><span class="fui-check"></span></button>
+                      </span>
+                    </div>
+                    <div id="slider" onclick="getPercent(this);"><p class="percent">--</p></div>
+                <table class="table table-hover  table-bordered">
+                     <thead>
+                         <td>Умение</td>
+                         <td>Навык</td>
+                     </thead>
+                    <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">10%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-danger" style="width: 10%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                     
+                      <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">50%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-warning" style="width: 50%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                     
+                      <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">60%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-success" style="width: 60%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                </table>
+            </div>
+            <div class="col-md-6 text-center">
+                <h3>Хочу выучить:</h3>
+                    <div class="input-group">
+                      <input type="text" class="form-control" placeholder="Добавить" id="search-query-3">
+                      <span class="input-group-btn">
+                        <button type="submit" class="btn"><span class="fui-check"></span></button>
+                      </span>
+                    </div>
+               <div id="slider2" onclick="getPercent(this);"><p class="percent">--</p></div>
+                <table class="table table-hover  table-bordered">
+                     <thead>
+                         <td>Умение</td>
+                         <td>Навык</td>
+                     </thead>
+                      <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">10%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-danger" style="width: 10%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                    
+                      <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">50%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-warning" style="width: 50%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                     
+                       <tr>
+                           <td class="vert_mid">Javascript</td>
+                           <td>
+                               <div>
+                                  <label class="skill_value" for="">60%</label>
+                                   <div class="progress">
+                                         <div class="progress-bar progress-bar-success" style="width: 60%;"></div> 
+                                    </div>
+                               </div>
+                          </td>
+                     </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+</section>
+<script>
+var $slider = $("#slider");
+var $slider2 = $("#slider2");
+if ($slider.length > 0) {
+  $slider.slider({
+    min: 0,
+    max: 10,
+    value: 5,
+    orientation: "horizontal",
+    range: "min"
+  }),$slider2.slider({
+    min: 0,
+    max: 10,
+    value: 5,
+    orientation: "horizontal",
+    range: "min"
+  })
+}
+    
+    
+var getPercent = function (node){
+   var nodeLength = node.childNodes[1];
+   var sliderWidth = parseInt($("#slider").css("width"));
+   var percent = parseInt($(nodeLength).css("width"))/sliderWidth*100;
+   var textHolder = node.childNodes[0];
+   textHolder.innerHTML = percent.toFixed()+"%";
+}
+
+//var func = $(".ui-slider-range").on('mouseout',function(){
+//    var sliderWidth = parseInt($("#slider").css("width"));
+//    var percent = parseInt($(this).css("width"))/sliderWidth*100;
+//    console.log(percent.toFixed()+"%");
+//    
+//  console.log($(this)[0]);
+//    
+//})
+
+
+
+
+</script>
 @stop
